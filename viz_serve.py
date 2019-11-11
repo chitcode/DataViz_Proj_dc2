f53bdb2ad76537300463bba062a4d85dc14126e9
Chitrasen Mohanty <mohantychitrasen@gmail.com>
2019-11-11 02:06:55 -0600

    added host and port

--- viz_serve.py
+++ viz_serve.py
@@ -26,7 +26,7 @@ print("data loaded")
 def index():
     return render_template("index.html")
 
-@app.route('/favicon.ico')
+@app.route('/favicon.ico',methods=['GET'])
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'connection.png', mimetype='image/connection.png')
 
@@ -247,4 +247,5 @@ def calcualte_moving_averages_single(prod):
 if __name__ == '__main__':
     init();
     os.environ["FLASK_ENV"] = 'development'
-    app.run(threaded=True,port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
